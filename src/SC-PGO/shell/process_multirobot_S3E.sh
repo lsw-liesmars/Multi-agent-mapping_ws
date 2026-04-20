8bdf75cc87f590415c64711b02cdc4a65256b3dd
lvshangwei <lv20140901@outlook.com>
2026-04-20 20:41:11 +0800

    feat: 重装系统后修改

--- src/SC-PGO/shell/process_multirobot_S3E.sh
+++ src/SC-PGO/shell/process_multirobot_S3E.sh
@@ -7,7 +7,7 @@ rm -rf /home/cyw/temp/failed_quatro.txt
 
 # # ##
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-source /home/lsw/Multi-agent-mapping_ws/devel/setup.bash
+source /home/lvshangwei/Multi-agent-mapping_ws/devel/setup.bash
 
 # # Start roscore in background if not already running
 # roscore &
@@ -17,8 +17,8 @@ roslaunch aloam_velodyne aloam_vlp16_S3E.launch &
 
 sleep 2
 
-rosbag play /home/lsw/lvshangwei/cyw_handover/S3E_Playground_2/data1.bag -r 1.0 -s 0 -q &
-rosbag play /home/lsw/lvshangwei/cyw_handover/S3E_Playground_2/data2.bag -r 1.0 -s 0 -q &
-rosbag play /home/lsw/lvshangwei/cyw_handover/S3E_Playground_2/data3.bag -r 1.0 -s 0 -q &
+rosbag play /home/lvshangwei/lvshangwei/cyw_handover/S3E_Playground_2/data1.bag -r 1.0 -s 0 -q &
+rosbag play /home/lvshangwei/lvshangwei/cyw_handover/S3E_Playground_2/data2.bag -r 1.0 -s 0 -q &
+rosbag play /home/lvshangwei/lvshangwei/cyw_handover/S3E_Playground_2/data3.bag -r 1.0 -s 0 -q &
 
 wait
